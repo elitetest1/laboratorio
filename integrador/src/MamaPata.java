1c3fcec09f4b075fc50db22a588011703e230019
elitetest <tizzyprove@gmail.com>
2025-08-20 20:42:10 -0300

    Revert "MamaPata: error forzado"
    This reverts commit 4842750344e56947a12b4d47869b63e7a33cb2d2.

--- integrador/src/MamaPata.java
+++ integrador/src/MamaPata.java
@@ -1,7 +1,7 @@
 package RepasoJava3.integrador.src;
 
 public interface MamaPata {
-    double PICOss = 6.8; // cm
+    double PICO = 6.8; // cm
     double PATAS = 7.5; // cm
     void tocarOrgano();
 
