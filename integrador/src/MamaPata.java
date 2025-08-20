4842750344e56947a12b4d47869b63e7a33cb2d2
elitetest <tizzyprove@gmail.com>
2025-08-20 20:40:07 -0300

    MamaPata: error forzado

--- integrador/src/MamaPata.java
+++ integrador/src/MamaPata.java
@@ -1,7 +1,7 @@
 package RepasoJava3.integrador.src;
 
 public interface MamaPata {
-    double PICO = 6.8; // cm
+    double PICOss = 6.8; // cm
     double PATAS = 7.5; // cm
     void tocarOrgano();
 
